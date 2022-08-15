d270ba51f51139ab6c808239d7890326df0d26d2
Engjell <40763@edu.raseko.fi>
2022-08-15 23:04:11 +0300

    dustpress installation

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '116c1998be30fcd3ad3d4ae47fda18fd8165f809',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '116c1998be30fcd3ad3d4ae47fda18fd8165f809',
+    ),
+    'devgeniem/dustpress' => 
+    array (
+      'pretty_version' => '1.27.1',
+      'version' => '1.27.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b270508d1c197ab86fa591609a9a8104e6d138a5',
+    ),
+  ),
+);
