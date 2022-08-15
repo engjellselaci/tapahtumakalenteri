116c1998be30fcd3ad3d4ae47fda18fd8165f809
Engjell <40763@edu.raseko.fi>
2022-08-15 23:03:16 +0300

    wordpress setup and theme creation

---
+++ wp-content/themes/tapahtumakalenteri_teema/footer.php
@@ -0,0 +1,10 @@
+<footer id="footer" class="site-footer">
+
+    <p><?php bloginfo('name');?></p>
+
+    <h1>TEST</h1>
+    <?php wp_footer();?>
+</footer>
+</body>
+
+</html>
---
+++ wp-content/themes/tapahtumakalenteri_teema/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fi">
+
+<head>
+    <?php wp_head() ;?>
+</head>
+
+<body <?php body_class();?>>
+    <header class="site-header">
+        <h1><?php bloginfo( 'name' ) ;?></h1>
+    </header>
